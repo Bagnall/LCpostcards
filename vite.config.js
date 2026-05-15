e658047c2c02ee9d7834e7f73c1227bc600b67e8
RichardBagnall <github@bagnall.co.uk>
2026-05-15 17:26:07 +0100

    New cards and GA added

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,56 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				index: resolve(__dirname, "index.html"),
-				englishConversationHours: resolve(__dirname, "english-conversation-hours.html"),
-				intermediateConversationHours: resolve(__dirname, "intermediate-conversation-hours.html"),
-				advancedConversationHours: resolve(__dirname, "advanced-conversation-hours.html"),
-				conversationExchange: resolve(__dirname, "conversation-exchange.html"),
-				friendsWithoutFrontiers: resolve(__dirname, "friends-without-frontiers.html"),
-				languageAdvisingStudyAbroad: resolve(__dirname, "language-advising-study-abroad.html")
+
+				englishConversationHours: resolve(
+					__dirname,
+					"english-conversation-hours.html"
+				),
+
+				intermediateConversationHours: resolve(
+					__dirname,
+					"intermediate-conversation-hours.html"
+				),
+
+				advancedConversationHours: resolve(
+					__dirname,
+					"advanced-conversation-hours.html"
+				),
+
+				conversationExchange: resolve(
+					__dirname,
+					"conversation-exchange.html"
+				),
+
+				friendsWithoutFrontiers: resolve(
+					__dirname,
+					"friends-without-frontiers.html"
+				),
+
+				languageAdvisingStudyAbroad: resolve(
+					__dirname,
+					"language-advising-study-abroad.html"
+				),
+
+				oneToOneLanguageAdvisingAppointments: resolve(
+					__dirname,
+					"one-to-one-language-advising-appointments.html"
+				),
+
+				japaneseConversationHours: resolve(
+					__dirname,
+					"japanese-conversation-hours.html"
+				),
+
+				greekConversationHours: resolve(
+					__dirname,
+					"greek-conversation-hours.html"
+				),
+
+				portugueseConversationHours: resolve(
+					__dirname,
+					"portuguese-conversation-hours.html"
+				)
 			}
 		}
 	}
